332dfd597463d63d4cc15a69807ede1b909acd48
Kelvin Zhang <zhangxp1998@gmail.com>
2018-01-06 10:08:01 -0500

    show 2 decimal places of file size

--- monitor_up.py
+++ monitor_up.py
@@ -90,7 +90,7 @@ def sizeof_fmt(num, suffix='B'):
     """
     for unit in ['', 'K', 'M', 'G', 'T', 'P', 'E', 'Z']:
         if abs(num) < 1024.0:
-            return "%3.1f%s%s" % (num, unit, suffix)
+            return "%3.2f%s%s" % (num, unit, suffix)
         num /= 1024.0
     return "%.1f%s%s" % (num, 'Yi', suffix)
 
