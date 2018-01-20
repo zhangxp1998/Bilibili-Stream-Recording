4aa4b4f5abedd43b9fda5b149c6a44d140887896
Kelvin Zhang <zhangxp1998@gmail.com>
2018-01-20 10:31:37 -0500

    Make newspring script generic by read commandline args

--- newspring.py
+++ newspring.py
@@ -1,4 +1,3 @@
-import json
 import sys
 
 import requests
@@ -6,16 +5,19 @@ import requests
 
 def purchase_gift(award_id):
     HEADERS = {
-        'Cookie': sys.argv[1],
+        'Cookie': sys.argv[3],
         'Content-Type': 'application/x-www-form-urlencoded',
         'Accept-Encoding': 'gzip, deflate, br'}
-    PAYLOAD = {'award_id': award_id, 'exchange_num': 50}
-    resp = requests.post('http://api.live.bilibili.com/activity/v1/NewSpring/redBagExchange', data=PAYLOAD)
+    PAYLOAD = {'award_id': award_id, 'exchange_num': int(sys.argv[2])}
+    resp = requests.post('http://api.live.bilibili.com/activity/v1/NewSpring/redBagExchange', data=PAYLOAD, headers=HEADERS)
     data = resp.json()
     return data
 
 
 if __name__ == '__main__':
+    if len(sys.argv) < 4:
+        print('Usage: %s award_id count cookie' % (sys.argv[0], ))
+        sys.exit(0)
     while True:
-        data = purchase_gift('gift-109')
+        data = purchase_gift(sys.argv[1])
         print(data['msg'])
