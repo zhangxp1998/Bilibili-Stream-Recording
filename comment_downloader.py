6fc35b5747cfc4894ac3da5e07b1c4520078d220
Kelvin Zhang <zhangxp1998@gmail.com>
2018-02-13 13:44:38 -0500

    syntax typoe, catch exceptions

--- comment_downloader.py
+++ comment_downloader.py
@@ -25,7 +25,7 @@ async def download_comments(room_id, save_path):
             ]
             await asyncio.wait(tasks)
             # loop.run_until_complete(asyncio.wait(tasks))
-        except KeyboardInterrupt, asyncio.CancelledError:
+        except (KeyboardInterrupt, asyncio.CancelledError):
             print('Closing...')
             danmuji.close()
 
