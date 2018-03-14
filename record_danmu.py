dc0e4cc7b0f9db35d57cecd51e6c5fe0ddf75c27
Kelvin Zhang <zhangxp1998@gmail.com>
2018-03-14 16:34:43 -0400

    restart comment recorder every day

--- record_danmu.py
+++ record_danmu.py
@@ -4,9 +4,15 @@ import sys
 
 def main():
     room_id = sys.argv[1]
-    comment_path = sys.argv[2]
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(download_comments(room_id, comment_path))
+    while True:
+        prefix =  'gift-log'
+        if not os.path.exists(prefix):
+            os.makedirs(prefix)
+        comment_path = prefix + '/' + '[' + room_id + '] ' + datetime.now().strftime('%Y-%m-%d %H:%M') + '.xml'
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(asyncio.wait_for(download_comments(room_id, comment_path), timeout=60*60*24))
+        Thread(target=google_drive.upload_to_google_drive, args=(comment_path, True)).start()
+
 
 if __name__ == '__main__':
     while True:
