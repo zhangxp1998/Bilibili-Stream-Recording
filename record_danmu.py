76d221d8c2105952da3f0dba7bb2d4d4e5973516
Kelvin Zhang <zhangxp1998@gmail.com>
2018-03-14 16:40:47 -0400

    multiplex recording script

--- record_danmu.py
+++ record_danmu.py
@@ -2,24 +2,24 @@ import asyncio
 from comment_downloader import *
 from threading import Thread
 import sys
-from google_drive import upload_to_google_drive
+import google_drive
 
-def main():
-    room_id = sys.argv[1]
+async def main(room_id):
     while True:
         prefix =  'gift-log'
         if not os.path.exists(prefix):
             os.makedirs(prefix)
         comment_path = prefix + '/' + '[' + room_id + '] ' + datetime.now().strftime('%Y-%m-%d %H:%M') + '.xml'
-        loop = asyncio.get_event_loop()
-        loop.run_until_complete(asyncio.wait_for(download_comments(room_id, comment_path), timeout=60*60*24))
+        await asyncio.wait_for(download_comments(room_id, comment_path), timeout=60*60*24)
         Thread(target=google_drive.upload_to_google_drive, args=(comment_path, True)).start()
 
 
 if __name__ == '__main__':
+    loop = asyncio.get_event_loop()
     while True:
         try:
-            main()
+            tasks = [main(room_id) for room_id in sys.argv[1:]]
+            loop.run_until_complete(asyncio.wait(tasks))
         except KeyboardInterrupt:
             sys.exit(0)
         except:
